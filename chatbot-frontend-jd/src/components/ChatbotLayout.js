25d3ee122469dc16bc792efb7fb1e69860068587
eacdc <ea@cdcprinters.com>
2025-05-07 13:57:56 +0530

    Simplify audio message display and remove unnecessary UI elements in both frontends

--- chatbot-frontend-jd/src/components/ChatbotLayout.js
+++ chatbot-frontend-jd/src/components/ChatbotLayout.js
@@ -1818,16 +1818,10 @@ export default function ChatbotLayout({ children }) {
                                           className="h-10 w-full max-w-[250px] opacity-90" 
                                           preload="metadata"
                                         />
-                                        <div className="text-xs text-blue-100 mt-1 flex items-center">
-                                          <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3 mr-1 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor">
-                                            <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-                                          </svg>
-                                          <span>Audio message saved</span>
-                                        </div>
                                       </div>
                                     ) : (
                                       <div className="text-xs text-blue-100">
-                                        [Audio message - Loading...]
+                                        [Audio message]
                                       </div>
                                     )}
                                   </div>
